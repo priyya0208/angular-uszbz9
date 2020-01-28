11da3b9129fa94a429d24c7116ccf58b17709208
priyya0208 <priyya.0208@gmail.com>
2020-01-28 16:44:48 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { appRoutes } from './routerConfig';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HomeComponent } from './home/home.component';
@@ -9,7 +11,7 @@ import { AboutComponent } from './about/about.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(appRoutes) ],
   declarations: [ AppComponent, HelloComponent, HomeComponent, AboutComponent, DashboardComponent ],
   bootstrap:    [ AppComponent ]
 })
---
+++ src/app/routerConfig.ts
@@ -0,0 +1,21 @@
+
+import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+export const appRoutes : Routes =  [
+  {
+    path: 'home',
+    component: 'HomeComponent'
+  },
+  {
+    path: 'about',
+    component: 'AboutComponent'
+  },
+  {
+    path: 'dashboard',
+    component: 'DashboardComponent'
+  }
+];
